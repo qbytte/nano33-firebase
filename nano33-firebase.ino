8f1301d432794a57e3695bc00ded7327af88605f
Omar Loyo <omarloyo121@gmail.com>
2022-06-19 02:01:52 -0600

    created function to handle temeperature readings sending data to firebase and implemented it

--- nano33-firebase.ino
+++ nano33-firebase.ino
@@ -1,15 +1,18 @@
 #include "arduino_secrets.h"
 
+// Temperature sensor library
+#include "DHT.h"
+
 // Firebase Arduino based on WiFiNINA - Version: Latest
 #include "Firebase_Arduino_WiFiNINA.h"
 
 
-char ssid[] = SECRET_SSID;      // your network SSID (name)
-char pass[] = SECRET_PASS;      // your network password (use for WPA, or use as key for WEP)
-int status = WL_IDLE_STATUS;    // the Wi-Fi status
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+int status = WL_IDLE_STATUS;
 
-char db_url[] = SECRET_DB_URL;  // firebase project url
-char db_sec[] = SECRET_DB_SEC;  // firebase secret to get access
+char db_url[] = SECRET_DB_URL;
+char db_sec[] = SECRET_DB_SEC;
 
 // define Firebase data object
 FirebaseData fbdo;
@@ -17,12 +20,25 @@ FirebaseData fbdo;
 // firebase endpoint
 String path = "/";
 
+// define sensor type and data pin
+#define DHTTYPE DHT11
+#define DHTPIN 2
+
+// initialize temp sensor
+DHT dht(DHTPIN, DHTTYPE);
+
 // led pin variable
 int led = 2;
+
+// time flag used for temperature readings
+int timeFlag = 0;
 
 void setup() {
   // initialize pins
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // initialize temp sensor
+  dht.begin();
   
   // initialize serial and wait for port to open:
   Serial.begin(9600);
@@ -32,14 +48,10 @@ void setup() {
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to network: ");
     Serial.println(ssid);
-    // connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
     delay(10000);
   }
-
-  // you're connected now, so print out the data:
+  
   Serial.println("You're connected to the network");
 
   Serial.println("---------------------------------------");
@@ -57,11 +69,19 @@ void setup() {
   // establish connection with firebase
   Firebase.begin(db_url, db_sec, ssid, pass);
   Firebase.reconnectWiFi(true);
+
+  // initial temperature reading
+  handleTempStatus();
 }
 
 void loop() {
   handleLedStatus();
+  if (timeFlag == 60) {
+    handleTempStatus();
+    timeFlag = 0;
+  }
   delay(1000);
+  timeFlag++;
 }
 
 void handleLedStatus() {
@@ -76,5 +96,32 @@ void handleLedStatus() {
     Serial.print("Firebase status: ");
     Serial.println(status);
     digitalWrite(LED_BUILTIN, LOW);
+  }
+}
+
+void handleTempStatus() {
+  float c = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  float h = dht.readHumidity();
+
+  String celcius = "";
+  celcius.concat(c);
+
+  String fahrenheit = "";
+  fahrenheit.concat(f);
+
+  String humidity = "";
+  humidity.concat(h);
+
+  String updateData =  "{\"c\":" + celcius + ", \"f\":" + fahrenheit + ", \"h\":" + humidity + "}";
+
+  if (Firebase.updateNode(fbdo, "/temp", updateData)) {
+    Serial.print("Returned data: ");
+    Serial.println(fbdo.jsonData());
+
+    Serial.print("Data sent: ");
+    Serial.println(updateData);
+  } else {
+    Serial.println(fbdo.errorReason());
   }
 }
